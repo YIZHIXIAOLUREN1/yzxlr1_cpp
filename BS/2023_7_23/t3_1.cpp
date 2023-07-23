10c3edab5f0dff3243820c4159f2bff19b3fad62
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-07-23 15:52:46 +0800

    1

--- BS/2023_7_23/t3_1.cpp
+++ BS/2023_7_23/t3_1.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define pr pair<int,ll>
+//#define pr pair<int,ll>
+#define prr pair<int,pair<int,ll>>
 #define inf 0x3f3f3f3f3f3f3f3f
 inline int read(){
     int res=0,f=1;
@@ -18,78 +19,71 @@ int num[N],s[N];
 ll xs[N],ss[N],re[N],ans=inf;
 vector<int> G[N];
 
-bool cmp(pr a,pr b){
-    return a.second>b.second;
+bool cmp(prr a,prr b){
+    if(a.second.first!=b.second.first)return a.second.first<b.second.first;
+    return a.second.second>b.second.second;
 }
 
 void dfs1(int u,int fa){
-	//printf("1:%d\n",u);//
     num[u]=1;
     s[u]=a[u];
-    vector<pr> b;
+    vector<prr> b;
     b.clear();
     for(auto v:G[u]){
         if(v==fa)continue;
         dfs1(v,u);
         num[u]+=num[v];
         s[u]+=s[v];
-        b.push_back({v,s[v]});
+        b.push_back({v,{num[v],s[v]}});
     }
-    if(fa!=0)b.push_back({fa,ns-s[u]});
+    if(fa!=0)b.push_back({fa,{n-num[u],ns-s[u]}});
     sort(b.begin(),b.end(),cmp);
     ll p=1;
     xs[u]=a[u];
     for(int i=0;i<b.size();i++){
         if(b[i].first==fa)continue;
-        xs[u]+=xs[b[i].first]+p*b[i].second;
-        p+=num[b[i].first];
+        xs[u]+=xs[b[i].first]+p*b[i].second.second;
+        p+=b[i].second.first;
     }
 }
 
 void dfs2(int u,int fa){
-	//printf("1:%d\n",u);//
-    vector<pr> b;
+    vector<prr> b;
     b.clear();
     for(auto v:G[u]){
         if(v==fa)continue;
-        b.push_back({v,s[v]});
+        b.push_back({v,{num[v],s[v]}});
     }
-    if(fa!=0)b.push_back({fa,ns-s[u]});
+    if(fa!=0)b.push_back({fa,{n-num[u],ns-s[u]}});
     sort(b.begin(),b.end(),cmp);
     ll p=1;
     re[u]=a[u];
     for(int i=0;i<b.size();i++){
-        if(b[i].first==fa){
-            re[u]+=ss[b[i].first]+p*b[i].second;
-            p+=n-num[u];
-        }
-        else{
-        	re[u]+=xs[b[i].first]+p*b[i].second;
-        	p+=num[b[i].first];
-        }
+        if(b[i].first==fa)
+            re[u]+=ss[b[i].first]+p*b[i].second.second;
+        else 
+            re[u]+=xs[b[i].first]+p*b[i].second.second;
+        p+=b[i].second.first;
     }
     ans=min(ans,re[u]);
-    printf("%d:%lld %lld %d %d\n",u,re[u],xs[u],s[u],num[u]);//
+    //printf("%d:%lld %lld %d %d\n",u,re[u],xs[u],s[u],num[u]);//
     for(auto v:G[u]){
         if(v==fa)continue;
         b.clear();
         for(auto vv:G[u]){
             if(vv==v||vv==fa)continue;
-            b.push_back({vv,s[vv]});
+            b.push_back({vv,{num[vv],s[vv]}});
         }
-        if(fa!=0)b.push_back({fa,ns-s[u]});
+        if(fa!=0)b.push_back({fa,{n-num[u],ns-s[u]}});
         sort(b.begin(),b.end(),cmp);
         p=1;
         ss[u]=a[u];
         for(int i=0;i<b.size();i++){
-            if(b[i].first==fa){
-                ss[u]+=ss[b[i].first]+p*b[i].second;
-                p+=n-num[u];
-            }
-            else{
-            	ss[u]+=xs[b[i].first]+p*b[i].second;
-            	p+=num[b[i].first];
-            }
+            if(b[i].first==fa)
+            ss[u]+=ss[b[i].first]+p*b[i].second.second;
+        else 
+            ss[u]+=xs[b[i].first]+p*b[i].second.second;
+        p+=b[i].second.first;
         }
         //printf("%d %d:%lld\n",u,v,ss[u]);//
         dfs2(v,u);
