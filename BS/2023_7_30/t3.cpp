bdd57e8d40e12103c92398231f912932f15f4780
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-07-30 15:30:08 +0800

    1

---
+++ BS/2023_7_30/t3.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=5e5+7;
+int n;
+vector<int> G[N];
+bool vis[N];
+
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+}
