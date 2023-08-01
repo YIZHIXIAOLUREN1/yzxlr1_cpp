f9ea521166199277e2c7089883a89e2c2d3292fe
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-01 14:48:03 +0800

    1

--- P3373.cpp
+++ P3373.cpp
@@ -2,111 +2,88 @@
 using namespace std;
 #define LL long long
 #define MAXN 300000
-const int mod=38;
+#define ls (u<<1)
+#define rs ((u<<1)|1)
+#define mid ((l+r)>>1)
+#define len (r-l+1)
+const int mod=571373;
 int n,m;
 int num[MAXN];
 LL w[MAXN];
-bool lac[MAXN],lah[MAXN];
 LL lazyc[MAXN],lazyh[MAXN];
 int umax=0;
 
 inline void pushup(const int u){
-    w[u]=w[2*u]+w[2*u+1]%mod;
+    w[u]=(w[ls]+w[rs])%mod;
 }
 
-void maketag(int u,int len,LL xh,LL xc){
-	if(xc!=0){
-		w[u]=w[u]*(xc%mod)%mod;
-		lazyc[u]*(xc%mod)%mod;
-		lac[u]=1;
-		lazyh[u]=lazyh[u]*(xc%mod)%mod;
-	}
-	else{
-		lah[u]=1;
-		lazyh[u]=(lazyh[u]+xh)%mod;
-	}
+void maketag(int u,int l,int r,LL xh,LL xc){
+	lazyc[u]=lazyc[u]*xc%mod;
+	lazyh[u]=(lazyh[u]*xc+xh)%mod;
+	w[u]=(w[u]*xc%mod+xh*len%mod)%mod;
+	//printf("%d:%d %d:%d %d:%d\n",u,l,r,xh,xc,w[u]);//
 }
 
-void pushdown(int u,int L,int R){
-	int M=(L+R) >> 1;
-	maketag(u*2,M-L+1,0,lazyc[u]);
-	maketag(u*2,M-L+1,lazyh[u],0);
-	maketag(u*2+1,R-M+1,0,lazyc[u]);
-	maketag(u*2+1,R-M+1,lazyh[u],0);
+void pushdown(int u,int l,int r){
+	maketag(ls,l,mid,lazyh[u],lazyc[u]);
+	maketag(rs,mid+1,r,lazyh[u],lazyc[u]);
 	lazyh[u]=0;
 	lazyc[u]=1;
 }
 
-void build(const int u,int L,int R){
+void build(int u,int l,int r){
 	umax++;
-    if(L==R){
-        w[u]=num[L];
+	lazyc[u]=1;
+    if(l==r){
+        w[u]=num[l];
         return;
     }
-    int M=(L+R) >> 1;
-    
-    build(u*2,L,M);
-    build(u*2+1,M+1,R);
+    build(ls,l,mid);
+    build(rs,mid+1,r);
     pushup(u);
 }
-
-inline bool inr(const int L,int R,int l,int r){
-    return (l<=L&&R<=r);
-}
-inline bool outr(const int L,int R,int l,int r){
-    return (L>r||R<l);
-}
-
-LL 	query(const int u,int L,int R,int l,int r){
-	if(inr(L,R,l,r))
-		return w[u];
-	if(!outr(L,R,l,r)){
-		int M=(L+R) >> 1;
-		pushdown(u,L,R);
-		return query(u*2,L,M,l,r)+query(u*2+1,M+1,R,l,r);
-	}
-	return 0;
+LL query(int u,int l,int r,int L,int R){
+	if(L<=l&&r<=R)return w[u];
+    if(l>R||r<L)return 0;
+	pushdown(u,l,r);
+	return (query(ls,l,mid,L,R)+query(rs,mid+1,r,L,R))%mod;
 }
 
-void update1(int u,int L,int R,int l,int r,LL xh,LL xc){
-	if(inr(L,R,l,r)){
-		maketag(u,R-L+1,0,xc);
-		maketag(u,R-L+1,xh,0);
+void update1(int u,int l,int r,int L,int R,LL xh,LL xc){
+	//printf("%d:%d %d:%d %d %d %d\n",u,l,r,L,R,xh,xc);//
+	if(L<=l&&r<=R){
+		maketag(u,l,r,xh,xc);
 		return;
 	}
-	if(!outr(L,R,l,r)){
-		int M=(L+R) >> 1;
-		pushdown(u,L,R);
-		update1(u*2,L,M,l,r,xh,xc);
-		update1(u*2,M+1,R,l,r,xh,xc);
-		pushup(u);
-		return;
-	}
-	return;
+	if(l>R||r<L)return;
+	pushdown(u,l,r);
+	update1(ls,l,mid,L,R,xh,xc);
+	update1(rs,mid+1,r,L,R,xh,xc);
+	pushup(u);
 }
 
 int main(){
-	//ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     scanf("%d%d%d",&n,&m,&num[0]);
     for(int i=1;i<=n;i++)
         scanf("%d",&num[i]);
     build(1,1,n);
     for(int i=0;i<m;++i){
+    	//for(int i=1;i<=umax;++i)cout << w[i] << " ";//
+    	//puts("");//
     	int node,xx,yy,kk;
     	scanf("%d",&node);
     	if(node==1){
     		scanf("%d%d%d",&xx,&yy,&kk);
-    		update1(1,1,n,xx,yy,0,kk);
+    		update1(1,1,n,xx,yy,0,kk%mod);
 		}
 		if(node==2){
     		scanf("%d%d%d",&xx,&yy,&kk);
-    		update1(1,1,n,xx,yy,kk,0);
+    		update1(1,1,n,xx,yy,kk%mod,1);
 		}
 		if(node==3){
     		scanf("%d%d",&xx,&yy);
     		cout << query(1,1,n,xx,yy) << endl;
 		}
-	//	for(int i=1;i<=umax;++i)cout << w[i] << " ";
 	}
     return 0;
 }
