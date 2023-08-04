45e3e692cf23d9f4a5856c98c7df83dc78ca56d8
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-04 14:44:16 +0800

    1

---
+++ BS/2023_8_4/t2.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mid ((l+r)>>1)
+const int N=1e5+7;
+int n,k;
+int deg[N];
+bool vis[N];
+int ud[N];
+vector<int> od,G[N];
+int res=0;
+void dfs(int u,int fa,int d){
+    if(!vis[u])vis[u]=1,res++;
+    ud[u]=d;
+    for(auto v:G[u]){
+        if(vis[v]&&d-ud[v]<2)continue;
+        if(v==fa)continue;
+        dfs(v,u,d-1);
+    }
+}
+
+bool check(int xa){
+    memset(vis,0,sizeof vis);
+    memset(ud,0,sizeof ud);
+    res=0;
+    for(auto u:od){
+        dfs(u,0,xa);
+        if(res>=n-k)return 1;
+    }
+    return 0;
+}
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n >> k;
+    for(int i=1,u,v;i<=n-1;i++){
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+        deg[u]++,deg[v]++;
+    }
+    for(int i=1;i<=n;i++)if(deg[i]==1)od.push_back(i);
+    int l=0,r=n;
+    while(l<=r){
+        if(check(mid))l=mid+1;
+        else r=mid-1;
+    }
+    cout << l;
+}
