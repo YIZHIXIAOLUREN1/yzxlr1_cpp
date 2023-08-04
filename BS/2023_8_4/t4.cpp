552579fadc76eb713d0581f3450ed65430c5fdb2
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-04 15:46:45 +0800

    1

--- BS/2023_8_4/t4.cpp
+++ BS/2023_8_4/t4.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 #define ll long long
 const int N=1e5+7;
-
+int n,m;
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
+    cin >> n >> m;
+    cout << 4*n-2*m;
 }
