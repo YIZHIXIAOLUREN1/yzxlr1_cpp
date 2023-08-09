4eab682c4f2e42d6f7f2cfe0129930fca889c4d3
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-09 15:16:13 +0800

    1

--- 0/线性筛.cpp
+++ 0/线性筛.cpp
@@ -4,15 +4,30 @@ using namespace std;
 const int N=1e5+7;
 int n;
 bool vis[N];
-int pri[N],tot=0;
+int p[N],tot=0,phi[N];
 
 
 void getprime(){
-    for(int i=2;i<=n;i++){
-        if(!vis[i])pri[++tot]=i;
-        for(int j=1;j<=tot&&(ll)pri[j]*i<=n;j++){
-            vis[i*pri[j]]=1;
-            if(i%pri[j]==0)break;
+    for(int i=2;i<N;i++){
+        if(!vis[i])p[++tot]=i;
+        for(int j=1;j<=tot&&(ll)p[j]*i<N;j++){
+            vis[i*p[j]]=1;
+            if(i%p[j]==0)break;
         }
     }
+}
+
+void pre(){
+	phi[1]=1;
+	for(int i=2;i<N;i++){
+		if(!vis[i])p[++p[0]]=i,phi[i]=i-1;
+		for(int j=1;j<=p[0]&&1ll*i*p[j]<N;j++){
+			vis[i*p[j]]=1;
+			if(i%p[j]==0){
+				phi[i*p[j]]=phi[i]*p[j];
+				break;
+			}
+			else phi[i*p[j]]=phi[i]*(p[j]-1);
+		}
+	}
 }
