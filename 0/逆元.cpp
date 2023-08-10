043f113c933aed511c8139ed4bf16911a2d6da0e
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-10 14:00:36 +0800

    1

--- 0/逆元.cpp
+++ 0/逆元.cpp
@@ -11,6 +11,14 @@ int qp(int x,int q){
     }
     return res;
 }
-int inv(int x){return qp(x,mod-2);}//费马小定理
+int inv1(int x){return qp(x,mod-2);}//费马小定理
 
-
+void exgcd(ll a,ll b,ll &x,ll &y){
+	if(a%b==0) x=0,y=1;
+	else exgcd(b,a%b,y,x),y-=a/b*x;
+}
+int inv(ll a,ll p){
+	ll x,y;
+	exgcd(a,p,x,y);
+	return (x%p+p)%p;
+}
