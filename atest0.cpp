530e68b0f1836b534f0bc14511268dcf6a058024
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-13 17:32:20 +0800

    1

--- atest0.cpp
+++ atest0.cpp
@@ -2,23 +2,8 @@
 using namespace std;
 #define ll long long
 const int N=1e7+7;
-int n;
-bool vis[N];
-int p[N/15],tot=0;
-
-
-void getprime(){
-    for(int i=2;i<N;i++){
-        if(!vis[i])p[++tot]=i;
-        for(int j=1;j<=tot&&(ll)p[j]*i<N;j++){
-            vis[i*p[j]]=1;
-            if(i%p[j]==0)break;
-        }
-    }
-}
 
 int main(){
-	getprime();
-	//cin >> n;
-	cout << tot;
+    set<int> q;
+    
 }
