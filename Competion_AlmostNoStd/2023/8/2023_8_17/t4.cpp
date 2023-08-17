6dce38302c6b8dde791d1809b5d145345769777d
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-17 16:02:19 +0800

    1

--- Competion_AlmostNoStd/2023/8/2023_8_17/t4.cpp
+++ Competion_AlmostNoStd/2023/8/2023_8_17/t4.cpp
@@ -14,33 +14,37 @@ struct node{
     int ans,lea;
     int la,lel,ra,ler;
     void ou(){
-    	printf("%d %d:%d:%d %d:%d %d:%d %d\n",
+    	printf("%d %d:%d:%d %d:%d %d:%d %d\n",//
     	l,r,cnt,ans,lea,la,lel,ra,ler);//
     }
 }w[N*4],ze;
 
+bool cmp(int a,int b,int c,int d){
+	return a*d>b*c;
+}
+
 node operator + (node a,node b){
-    if(a.ans==inf)return b;
-    if(b.ans==inf)return a;
+    if(a.la==inf)return b;
+    if(b.la==inf)return a;
     node res;
     res.l=a.l,res.r=b.r;
     res.cnt=a.cnt+b.cnt;
-    if((db)a.ans/a.lea>(db)b.ans/b.lea)res.ans=a.ans,res.lea=a.lea;
+    if(((db)a.ans)/a.lea>((db)b.ans)/b.lea)res.ans=a.ans,res.lea=a.lea;
     else res.ans=b.ans,res.lea=b.lea;
-    if((db)res.ans/res.lea<(db)(a.ra+b.la)/(a.ler+b.lel))
+    if(((db)res.ans)/res.lea<((db)(a.ra+b.la))/(a.ler+b.lel))
     res.ans=a.ra+b.la,res.lea=a.ler+b.lel;
 
-    if((db)a.la/a.lel>(db)(a.cnt+b.la)/(a.r-a.l+1+b.lel))res.la=a.la,res.lel=a.lel;
+    if(((db)a.la/a.lel)>((db)(a.cnt+b.la))/(a.r-a.l+1+b.lel))res.la=a.la,res.lel=a.lel;
     else res.la=a.cnt+b.la,res.lel=a.r-a.l+1+b.lel;
 
-    if((db)b.ra/b.ler>(db)(b.cnt+a.ra)/(b.r-b.l+1+a.ler))res.ra=b.ra,res.ler=a.ler;
+    if(((db)b.ra)/b.ler>((db)(b.cnt+a.ra))/(b.r-b.l+1+a.ler))res.ra=b.ra,res.ler=b.ler;
     else res.ra=b.cnt+a.ra,res.ler=b.r-b.l+1+a.ler;
 
+    if(res.lel>1&&((db)res.ans)/res.lea<((db)res.la)/res.lel)
+        res.ans=res.la,res.lea=res.lel;
+    if(res.ler>1&&((db)res.ans)/res.lea<((db)res.ra)/res.ler)
+        res.ans=res.ra,res.lea=res.ler;
     return res;
-}
-
-node max(node a,node b){
-    return (db)a.ans/a.lea>(db)b.ans/b.lea?a:b;
 }
 
 int laz[N*4];
@@ -68,24 +72,30 @@ void build(int u,int l,int r){
     if(l==r){
         w[u].l=w[u].r=l;
         w[u].lea=w[u].lel=w[u].ler=1;
-        w[u].cnt=w[u].ans=w[u].la=w[u].ra=a[l];
+        w[u].cnt=w[u].la=w[u].ra=a[l];
+        w[u].ans=inf;
         return;
     }
     build(ls,l,mid);
     build(rs,mid+1,r);
     pushup(u);
+    //printf("%d:%d %d:\n",u,l,r);//
+    //w[u].ou();//
 }
 
 void changeq(int u,int L,int R,int x){
+	//printf("%d:%d %d:%d:\n",u,L,R,x);//
     if(w[u].r<L||w[u].l>R)return;
     if(w[u].l>=L&&w[u].r<=R){
         maketag(u,x);
+        //w[u].ou();//
         return;
     }
     pushdown(u);
     changeq(ls,L,R,x);
     changeq(rs,L,R,x);
     pushup(u);
+    //w[u].ou();//
 }
 
 node query(int u,int L,int R){
@@ -97,8 +107,8 @@ node query(int u,int L,int R){
     	pushdown(u);
     	res=query(ls,L,R)+query(rs,L,R);
     }
-    printf("%d:\n",u);//
-    res.ou();
+    //printf("%d:\n",u);//
+    //res.ou();//
     return res;
 }
 
@@ -114,15 +124,18 @@ int main(){
     cin >> n >> m;
     for(int i=1;i<=n;i++)cin >> a[i];
     int ch,ll,rr,xx;
+    build(1,1,n);
     while(m--){
         cin >> ch >> ll >> rr;
         if(ch==1){
             cin >> xx;
             changeq(1,ll,rr,xx);
         }else{
+        	int fl=1;
             node an = query(1,ll,rr);
+            if(an.ans<0)fl=-1,an.ans=-an.ans;
             int g=gcd(an.ans,an.lea);
-            cout << an.ans/g << "/" << an.lea/g <<"\n";
+            cout << fl*an.ans/g << "/" << an.lea/g <<"\n";
         }
     }
 }
