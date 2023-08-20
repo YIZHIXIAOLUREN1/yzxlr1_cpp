bfc16cedf1917d5cb517c493931a9f6a4f3f279b
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-20 16:42:34 +0800

    1

---
+++ Competion_AlmostNoStd/2023/8/2023_8_20/t3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1007;
+int n,T;
+
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++)
+
+}
