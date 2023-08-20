1bc24aef4e773a46ac44bffe1bff942be6545625
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-20 16:36:53 +0800

    1

---
+++ Competion_AlmostNoStd/2023/8/2023_8_20/t4.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1e6+7,mod=998244353;
+int n;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+
+}
