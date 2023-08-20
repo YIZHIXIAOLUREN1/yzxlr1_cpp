f19542e19a8647447d168e4588d0671415089bcd
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-20 16:42:36 +0800

    1

--- Competion_AlmostNoStd/2023/8/2023_8_20/t4.cpp
+++ Competion_AlmostNoStd/2023/8/2023_8_20/t4.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 #define ll long long
 const int N=1e6+7,mod=998244353;
-int n;
+int n,ans;
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n;
-
+    for(int t=1;t<=n-1;t++){
+        ans=(ans+1ll*(t+1)*t/2%mod*(n-t)%mod)%mod;
+    }
+    cout << ans;
 }
