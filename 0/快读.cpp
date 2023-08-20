cfa7539b71317cdb18c9b3440939e0d30b3d490f
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-20 17:02:26 +0800

    1

--- 0/快读.cpp
+++ 0/快读.cpp
@@ -4,6 +4,7 @@
 
 #define gc() getchar()
 #define pc(x) putchar(x)
+#define endl pc(10)
 inline int read(){int w=1,q=0;char ch=' ';
     while(ch!='-'&&(ch<'0'||ch>'9'))ch=gc();if(ch=='-')w=-1,ch=gc();
     while(ch>='0'&&ch<='9')q=q*10+ch-'0',ch=gc();return w*q;}
--- Competion_AlmostNoStd/2023/8/2023_8_20/t3.cpp
+++ Competion_AlmostNoStd/2023/8/2023_8_20/t3.cpp
@@ -2,25 +2,33 @@
 using namespace std;
 #define ll long long
 const int N=1007;
+
+#define gc() getchar()
+#define pc(x) putchar(x)
+#define endl pc(10)
+inline int read(){int w=1,q=0;char ch=' ';
+    while(ch!='-'&&(ch<'0'||ch>'9'))ch=gc();if(ch=='-')w=-1,ch=gc();
+    while(ch>='0'&&ch<='9')q=q*10+ch-'0',ch=gc();return w*q;}
+inline void pr(ll x){if(x<0)pc('-'),x=-x;if(x>9)pr(x/10);pc(x%10+'0');}
+
 int n,T;
 int ax[N],ay[N];
-vector<int> G[N];
 int f[N][N];
 
 int main(){
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin >> n;
+    //ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    n=read();
     for(int i=1;i<=n;i++){
-        cin >> ax[i] >> ay[i];
+        ax[i]=read();
+        ay[i]=read();
     }
     memset(f,0x3f,sizeof f);
 
     for(int i=1;i<=n;i++){
         f[i][i]=0;
         for(int j=i+1;j<=n;j++){
-            if((abs(ax[j]-ax[i])==1&&ay[i]==ay[j])||(abs(ay[j]-ay[i])==1&&ax[i]==ax[j])){
-                G[i].push_back(j);
-                G[j].push_back(i);
+            if((abs(ax[j]-ax[i])==1&&ay[i]==ay[j])||
+            (abs(ay[j]-ay[i])==1&&ax[i]==ax[j])){
                 f[i][j]=f[j][i]=1;
             }
         }
@@ -35,11 +43,11 @@ int main(){
             }
         }
     }
-    cin >> T;
+    T=read();
     while(T--){
         int s,t;
-        cin >> s >> t;
-        cout<< (s<=t?f[s][t]:f[t][s]) << "\n";
+        s=read();t=read();
+        pr((s<=t?f[s][t]:f[t][s])),endl;
     }
 
 }
