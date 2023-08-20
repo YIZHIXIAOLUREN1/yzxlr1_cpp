c524449f30eededbb1a11ca92fdfa65ceef07f25
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-08-20 17:18:30 +0800

    1

--- Competion_AlmostNoStd/2023/8/2023_8_20/t4.cpp
+++ Competion_AlmostNoStd/2023/8/2023_8_20/t4.cpp
@@ -3,12 +3,11 @@ using namespace std;
 #define ll long long
 const int N=1e6+7,mod=998244353;
 int n,ans;
+vector<int> G[N];
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n;
-    for(int t=1;t<=n-1;t++){
-        ans=(ans+1ll*(t+1)*t/2%mod*(n-t)%mod)%mod;
-    }
-    cout << ans;
+    for(int i=1;i<=n-1;i++)
+
 }
