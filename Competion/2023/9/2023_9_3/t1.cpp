a7864efd97c9aeb78d5d1fc4a6b6aa436f8bb663
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-03 14:45:09 +0800

    1

--- Competion/2023/9/2023_9_3/t1.cpp
+++ Competion/2023/9/2023_9_3/t1.cpp
@@ -4,10 +4,29 @@ using namespace std;
 const int N=1e5+7;
 int n;
 int a[N],b[N];
-int dt[N];
-
+ll ans=0;
+int top=0,it;
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+    }
+    for(int i=1;i<=n;i++){
+        cin >> b[i];
+    }
 
+    for(int i=1;i<=n;i++){
+        if(b[i]<=a[i])ans+=a[i]-a[i-1];
+        else{
+            top=b[i];
+            for(int j=i+1;a[j]<=top;j++){
+                if(b[j]>top)top=b[j];
+                it=j;
+            }
+            
+        }
+    }
+    
 }
