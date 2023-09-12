96cd973b80a4c55c9cf9a9e05b000c5f3dc02004
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-12 14:07:22 +0800

    1

--- 0/0.cpp
+++ 0/0.cpp
@@ -1,24 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-signed main(void){}//Ç°¿É¼Ó #define int long long 
-//== 
-int main(){}
 
 
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-/*
-int pr(int n) {
-	int res=n;
-	for(int i =2; i*i <=n ; ++i) {
-		if(n%i==0) {
-			res=res/i*(i-1);
-			while(n%i==0) {
-				n/=i;
-			}
-		}
-	}
-	if(n>1) res=res/n*(n-1);
-	return res;
+    
 }
-*/
