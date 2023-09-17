f19affa50ddab1932e5d3ea407edd1680b20a9f8
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-17 17:20:24 +0800

    1

---
+++ P1441.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1e5+7;
+int n,m;
+int a[N];
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+}
