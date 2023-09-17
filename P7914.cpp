df3b79552587c4c4c74b35a5ca804faf4e0dcfea
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-17 11:43:33 +0800

    1

--- P7914.cpp
+++ P7914.cpp
@@ -2,21 +2,35 @@
 using namespace std;
 #define ll long long
 const int N=505,mod=1e9+7;
-int n,k;
-int a[N];
+int n,K;
+int a[N];//0->*,1->(,2->),3->?
+int pa[N],pi[N];
 int f[N][N][N];
 
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin >> n >> k;
+    cin >> n >> K;
+    string s;
+    cin >> s;
     for(int i=1;i<=n;i++){
-
+        switch(s[i-1]){
+            case '*':a[i]=0;break;
+            case '(':a[i]=1;break;
+            case ')':a[i]=2;break;
+            case '?':a[i]=3;break;
+        }
     }
-
+    for(int i=n;i>=1;i++){
+        pa[i]=pa[i+1]+(a[i]==2)+(a[i]==3)-(a[i]==1);
+        pi[i]=pi[i+1]+(a[i]==2)-(a[i]==3)-(a[i]==1);
+    }
 }
 /*
-
-f
-
+pre[i]->maxj
+f[i][j][k]->
+f[i+1][j+1][0],
+f[i+1][j-1][0],
+f[i+1][j][k+1],
+pi[i]<=j<=pa[i],k<=K
 */
