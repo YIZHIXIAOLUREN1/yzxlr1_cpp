c9cf4962771433a184aafda0e18400696c2bdb78
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-17 11:13:07 +0800

    1

---
+++ P7914.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=505,mod=1e9+7;
+int n,k;
+int a[N];
+int f[N][N][N];
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n >> k;
+    for(int i=1;i<=n;i++){
+
+    }
+
+}
+/*
+
+f
+
+*/
