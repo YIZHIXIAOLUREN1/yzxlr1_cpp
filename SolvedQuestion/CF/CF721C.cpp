115e410eb25857b0cc04989ba88f0d21160c08d7
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-18 13:58:17 +0800

    1

--- SolvedQuestion/CF/CF721C.cpp
+++ SolvedQuestion/CF/CF721C.cpp
@@ -12,7 +12,7 @@ bool vis[N];
 int ans[N];
 
 int main(){
-    //ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n >> m >> T;
     for(int i=1,u,v,t;i<=m;i++){
         cin >> u >> v >> t;
@@ -31,10 +31,8 @@ int main(){
                     for(int i=2;i<=n;i++){
                         if(f[v][i]>f[u][i-1]+t){
                             f[v][i]=f[u][i-1]+t;
-                            //printf("%d %d %d:%d\n",u,v,i,f[v][i]);//
                             g[v][i]=u;
                         }
-                        //f[v][i]=min(f[v][i],f[u][i-1]+t);
                     }
                     deg[v]--;
                 }
