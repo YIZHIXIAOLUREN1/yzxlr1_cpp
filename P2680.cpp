aa40b4ac4ab75171be48a130b1738ceda95ac755
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-21 13:40:04 +0800

    1

--- P2680.cpp
+++ P2680.cpp
@@ -15,6 +15,7 @@ ll a[N],b[N];
 ll wa[N*4],wx[N*4],laz[N*4];
 vector<pr> G[N];
 int id[N],son[N],fa[N],siz[N],top[N],idx,d[N];
+int res;
 
 void pushup(int u){wa[u]=(wa[ls]+wa[rs]);wx[u]=max(wx[ls],wx[rs]);}
 void build(int u,int l,int r){
@@ -24,12 +25,10 @@ void build(int u,int l,int r){
     pushup(u);
 }
 ll query1(int u,int l,int r,int L,int R){
-	int res=0;
-    if(l>R||r<L)res=0;
-    else if(l>=L&&r<=R)res=wa[u];
-    else res=(query1(ls,l,mid,L,R)+query1(rs,mid+1,r,L,R));
+    if(l>R||r<L)return 0;
+    if(l>=L&&r<=R)return wa[u];
+    return (query1(ls,l,mid,L,R)+query1(rs,mid+1,r,L,R));
     //printf("q:%d %d %d:%d %d:%d\n",u,l,r,L,R,res);//
-    return res;
 }
 ll query2(int u,int l,int r,int L,int R){
     if(l>R||r<L)return 0;
@@ -59,14 +58,14 @@ void dfs2(int u,int tof){
     if(siz[u]==1)return;
     dfs2(son[u],tof);
     for(auto ii:G[u]){
-        int v=ii.first,w=ii.second;
+        int v=ii.first;
         if(v==fa[u]||v==son[u])continue;
         dfs2(v,v);
     }
 }
 
 ll q1(int x,int y){
-    ll res=0;
+    res=0;
     if(d[x]<d[y])swap(x,y);
     while(top[x]!=top[y]){
         if(d[top[x]]<d[top[y]])swap(x,y);
@@ -76,12 +75,12 @@ ll q1(int x,int y){
     }
     if(d[x]<d[y])swap(x,y);
     res=(res+query1(1,1,n,id[y],id[x]));
-    res-=2*a[y];
+    res-=a[y];
     return res;
 }
 
 ll q2(int x,int y){
-    ll res=0;
+    res=0;
     if(d[x]<d[y])swap(x,y);
     while(top[x]!=top[y]){
         if(d[top[x]]<d[top[y]])swap(x,y);
@@ -105,8 +104,7 @@ int LCA(int x,int y){
 }
 
 bool cmp(prr x,prr y){return x.first>y.first;}
-int bd[10];
-bool cmp1(int x,int y){return d[x]>d[y];}
+
 int main(){
     //ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n >> m;
@@ -118,29 +116,31 @@ int main(){
     dfs1(1,0);
     dfs2(1,1);
     build(1,1,n);
-    for(int i=1;i<=n;i++){
-    	printf("%d:%d %d %d:%d %d\n",i,fa[i],son[i],siz[i],top[i],id[i]);//
-    }
+    // for(int i=1;i<=n;i++){
+    // 	printf("%d:%d %d %d:%d %d\n",i,fa[i],son[i],siz[i],top[i],id[i]);//
+    // }
     for(int i=1,u,v;i<=m;i++){
         cin>>u>>v;
         ys[i].second.first=u;
         ys[i].second.second=v;
         ys[i].first=q1(u,v);
-        printf("%d:%d %d:%d\n",i,u,v,ys[i].first);//
+        //printf("%d:%d %d:%d\n",i,u,v,ys[i].first);//
     }
     sort(ys+1,ys+m+1,cmp);
     int ans=ys[1].first-q2(ys[1].second.first,ys[1].second.first);
+    ans=max(ans,ys[2].first);
     int u=ys[1].second.first,v=ys[1].second.second;
     for(int i=2;i<=m;i++){
         int x=ys[i].second.first,y=ys[i].second.second;
-        if(id[x]>id[y])swap(x,y);
-        if(id[u]>id[v])swap(u,v);
+        if(id[x]>id[y]&&id[x]>id[y]+siz[y]-1)swap(x,y);
+        if(id[u]>id[v]&&id[u]>id[v]+siz[v]-1)swap(u,v);
         int uu=LCA(u,x);
         int vv=LCA(uu,v),v1=LCA(uu,y),v2=LCA(v,y);
         if(d[vv]<d[v1])vv=v1;
         if(d[vv]<d[v2])vv=v2;
+        res=q2(uu,vv);
+        //printf("%d %d:%d %d:%d %d:%d\n",u,v,x,y,uu,vv,res);//
         u=uu,v=vv;
-        int res=q2(uu,vv);
         if(ys[1].first-res>=ys[i+1].first){
             ans=min(ans,ys[1].first-res);
             break;
