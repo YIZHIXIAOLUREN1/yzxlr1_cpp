b75e7de1ac440b0a6f88905113ddb2d26ecb78db
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-26 14:09:38 +0800

    1

--- P1967.cpp
+++ P1967.cpp
@@ -1,20 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define ll int
 #define pr pair<int,int>
 #define prr pair<int,pr>
 #define ls (u<<1)
 #define rs ((u<<1)|1)
 #define mid ((l+r)>>1)
 #define len (r-l+1)
-const ll N=5e4+7,inf=0x3f3f3f3f;
+const ll N=1e4+7,inf=0x3f3f3f3f;
 int n,m,Q;
-int s[N],t[N];
+int s[3*N],t[3*N];
 int b[N];
 ll w[N*4];
 vector<pr> G[N],Gi[N];
-
 int id[N],son[N],ffa[N],siz[N],top[N],idx,d[N];
+prr gg[3*N];
+int fa[N];
 
 void dfs1(int u,int fas){
     ffa[u]=fas;
@@ -33,17 +34,26 @@ void dfs1(int u,int fas){
 void dfs2(int u,int tof){
     top[u]=tof;
     if(siz[u]==1)return;
+    id[son[u]]=++idx;
     dfs2(son[u],tof);
     for(auto ii:Gi[u]){
-        int v=ii.first,w=ii.second;
-        if(v==ffa[u]||v==son[u])continue;
+        int v=ii.first,ww=ii.second;
+        if(v==ffa[u])continue;
+        if(v==son[u]){
+        	b[id[son[u]]]=ww;
+        	// printf("%d %d:%d %d\n",u,v,id[son[u]],b[id[son[u]]]);//
+        	continue;
+        }
         id[v]=++idx;
-        b[idx]=w;
+        b[idx]=ww;
         dfs2(v,v);
     }
 }
 
-void pushup(int u){w[u]=min(w[ls],w[rs]);}
+void pushup(int u){
+	w[u]=min(w[ls],w[rs]);
+	//printf("%d:%d\n",u,w[u]);//
+}
 
 void build(int u,int l,int r){
     if(l==r){w[u]=b[l];return;}
@@ -53,6 +63,8 @@ void build(int u,int l,int r){
 }
 
 ll query(int u,int l,int r,int L,int R){
+	//printf("%d:%d %d:%d %d\n",u,l,r,L,R);//
+	if(R<L)return inf;
     if(l>R||r<L)return inf;
     if(l>=L&&r<=R)return w[u];
     return min(query(ls,l,mid,L,R),query(rs,mid+1,r,L,R));
@@ -64,16 +76,16 @@ ll LCA(int x,int y){
 		if(d[top[x]]<d[top[y]]){
 			swap(x,y);
 		}
+		//printf("%d %d:\n",x,y);//
         res=min(res,query(1,1,n,id[top[x]],id[x]));
 		x=ffa[top[x]];
 	}
 	if(d[x]<d[y])swap(x,y);
-    res=min(res,query(1,1,n,son[id[y]],id[x]));
+	//printf("%d %d:\n",x,y);//
+    res=min(res,query(1,1,n,id[son[y]],id[x]));
     return res;
 }
 
-prr gg[N];
-int fa[N];
 bool cmp(prr x,prr y){return x.first>y.first;}
 
 int find(int x){
@@ -85,10 +97,10 @@ void kru(){
     sort(gg+1,gg+m+1,cmp);
     for(int i=1;i<=m;i++){
         int u=gg[i].second.first,v=gg[i].second.second;
-        int w=gg[i].first;
+        int ww=gg[i].first;
         if(find(u)==find(v))continue;
-        Gi[u].push_back({v,w});
-        Gi[v].push_back({u,w});
+        Gi[u].push_back({v,ww});
+        Gi[v].push_back({u,ww});
         fa[u]=v;
     }
 }
@@ -96,21 +108,31 @@ void kru(){
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     cin >> n >> m;
-    for(int i=1,u,v,w;i<=m;i++){
-        cin >> u >> v >> w;
-        G[u].push_back({v,w});
-        G[v].push_back({u,w});
-        gg[i]={w,{u,v}};
+    for(int i=1,u,v,ww;i<=m;i++){
+        cin >> u >> v >> ww;
+        G[u].push_back({v,ww});
+        G[v].push_back({u,ww});
+        gg[i]={ww,{u,v}};
     }
+    cin >> Q;
+    for(int i=1;i<=Q;i++)cin >> s[i] >> t[i];
+    
     kru();
+    memset(b,0x3f,sizeof b);
+    memset(w,0x3f,sizeof w);
     for(int i=1;i<=n;i++){
         if(!siz[i]){
             dfs1(i,0);
+            id[i]=++idx;
             dfs2(i,i);
         }
     }
+    //for(int i=1;i<=n;i++){
+    //    printf("%d:%d %d %d:%d %d %d\n",
+    //    i,id[i],son[i],ffa[i],siz[i],top[i],d[i]);//
+    //}
     build(1,1,n);
-    for(int i=1;i<=m;i++){
+    for(int i=1;i<=Q;i++){
         if(find(s[i])!=find(t[i])){cout<<"-1\n";continue;}
         cout<<LCA(s[i],t[i])<<"\n";
     }
