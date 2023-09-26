cc29ada6f133bb112d8b06a0d77602fc3fb3da81
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-26 13:46:50 +0800

    1

--- P1967.cpp
+++ P1967.cpp
@@ -7,21 +7,21 @@ using namespace std;
 #define rs ((u<<1)|1)
 #define mid ((l+r)>>1)
 #define len (r-l+1)
-const int N=5e4+7;
+const ll N=5e4+7,inf=0x3f3f3f3f;
 int n,m,Q;
 int s[N],t[N];
 int b[N];
 ll w[N*4];
 vector<pr> G[N],Gi[N];
 
-int id[N],son[N],fa[N],siz[N],top[N],idx,d[N];
+int id[N],son[N],ffa[N],siz[N],top[N],idx,d[N];
 
 void dfs1(int u,int fas){
-    fa[u]=fas;
+    ffa[u]=fas;
     siz[u]=1;
     d[u]=d[fas]+1;
     int ms=0;
-    for(auto ii:G[u]){
+    for(auto ii:Gi[u]){
         int v=ii.first;
         if(v==fas)continue;
         dfs1(v,u);
@@ -34,25 +34,16 @@ void dfs2(int u,int tof){
     top[u]=tof;
     if(siz[u]==1)return;
     dfs2(son[u],tof);
-    for(auto ii:G[u]){
+    for(auto ii:Gi[u]){
         int v=ii.first,w=ii.second;
-        if(v==fa[u]||v==son[u])continue;
+        if(v==ffa[u]||v==son[u])continue;
         id[v]=++idx;
         b[idx]=w;
         dfs2(v,v);
     }
 }
 
-int LCA(int x,int y){
-	while(top[x]!=top[y]){
-		if(d[top[x]]<d[top[y]]){
-			swap(x,y);
-		}
-		x=fa[top[x]];
-	}
-	return (d[x]<d[y])?x:y;
-}
-void pushup(int u){w[u]=(w[ls]+w[rs]);}
+void pushup(int u){w[u]=min(w[ls],w[rs]);}
 
 void build(int u,int l,int r){
     if(l==r){w[u]=b[l];return;}
@@ -62,9 +53,23 @@ void build(int u,int l,int r){
 }
 
 ll query(int u,int l,int r,int L,int R){
-    if(l>R||r<L)return 0;
+    if(l>R||r<L)return inf;
     if(l>=L&&r<=R)return w[u];
-    return (query(ls,l,mid,L,R)+query(rs,mid+1,r,L,R));
+    return min(query(ls,l,mid,L,R),query(rs,mid+1,r,L,R));
+}
+
+ll LCA(int x,int y){
+    ll res=inf;
+	while(top[x]!=top[y]){
+		if(d[top[x]]<d[top[y]]){
+			swap(x,y);
+		}
+        res=min(res,query(1,1,n,id[top[x]],id[x]));
+		x=ffa[top[x]];
+	}
+	if(d[x]<d[y])swap(x,y);
+    res=min(res,query(1,1,n,son[id[y]],id[x]));
+    return res;
 }
 
 prr gg[N];
@@ -98,6 +103,16 @@ int main(){
         gg[i]={w,{u,v}};
     }
     kru();
-    
+    for(int i=1;i<=n;i++){
+        if(!siz[i]){
+            dfs1(i,0);
+            dfs2(i,i);
+        }
+    }
+    build(1,1,n);
+    for(int i=1;i<=m;i++){
+        if(find(s[i])!=find(t[i])){cout<<"-1\n";continue;}
+        cout<<LCA(s[i],t[i])<<"\n";
+    }
     return 0;
 }
