6eba779335152c2271e69cef652dbd2c4068e66d
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-27 13:37:34 +0800

    1

--- T206515.cpp
+++ T206515.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-
+const ll N=1000007,inf=0x3f3f3f3f;
 
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cout <<0x3f3f3f3f;
+    
     
     return 0;
 }
