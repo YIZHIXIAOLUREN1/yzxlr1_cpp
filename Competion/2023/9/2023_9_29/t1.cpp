d98b5e26783eae512e427027100e3d3b66b72ecd
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-29 14:23:41 +0800

    1

---
+++ Competion/2023/9/2023_9_29/t1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1e6+7;
+int n;
+ll a[N],b[N];
+int ans,l,r;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++)cin >> a[i];
+    for(int i=1;i<=n;i++)cin >> b[i];
+
+}
