cbf7f5054e7b6774e54a29bf821e3216fd5a8fc1
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-09-29 16:30:46 +0800

    1

---
+++ Competion/2023/9/2023_9_29/t2.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N=1e3+7,inf=0x3f3f3f3f3f3f3f3f;
+int n;
+int a[N];
+ll f[N][N];
+ll ans;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++)cin >> a[i];
+    memset(f,0x3f,sizeof f);
+    f[0][0]=0;
+    f[1][0]=a[1];
+    ans=inf;
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<i;j++){
+            if(i>j+1)f[i][j]=min(f[i][j],f[i-1][j]+abs(a[i]-a[i-1]));
+            else for(int k=1;k<=j;k++)
+                    f[i][j]=min(f[i][j],f[j][j-k]+abs(a[i]-a[j-k]));
+            if(i==n)ans=min(ans,f[i][j]);
+            //printf("%d %d:%d\n",i,j,f[i][j]);//
+        }
+    }
+    cout << ans;
+}
+/*
+f[i][j]<-f[i-1][j]+s[i][i-1],i>j+1
+f[i][j]<-f[j][j-k]+s[i][j-k],i=j+1,0<k<=j
+*/
