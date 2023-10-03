bc435428651137be0ce6b6e69481c9ebd88ff01f
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-10-03 16:28:55 +0800

    1

--- Competion/2023/10/2023_10_3/SC-708883/fastslow/fastslow.cpp
+++ Competion/2023/10/2023_10_3/SC-708883/fastslow/fastslow.cpp
@@ -3,22 +3,43 @@ using namespace std;
 #define ll long long
 const int N=1e5+7;
 int n,m;
-int a[N];
+int a[N],lsh[N];
+int rr[N],nxt[N],la[N];
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     freopen("fastslow.in","r",stdin);
     freopen("fastslow.out","w",stdout);
     cin >> n >> m;
-    for(int i=1;i<=n;i++)cin >> a[i];
+    for(int i=1;i<=n;i++){
+        cin >> a[i];
+        lsh[i]=a[i];
+    }
+    sort(lsh+1,lsh+n+1);
+    int cnt=unique(lsh+1,lsh+n+1)-lsh-1;
+    for(int i=1;i<=n;i++)a[i]=lower_bound(lsh+1,lsh+cnt+1,a[i])-lsh;
+
+    for(int i=1;i<=n;i++){rr[a[i]]++;la[i]=n+1;}
+    for(int i=n;i>=1;i--){
+        nxt[i]=la[a[i]];
+        la[a[i]]=i;
+    }
+    nxt[0]=n+1;
+    
     for(int i=1;i<=m;i++){
         int x,y;
         cin >> x >> y;
-        ll res=0,l=0,r=0;
-        for(int j=1;j<=n;j++)if(a[j]==y)r++;
-        for(int j=1;j<=n;j++){
+        x=lower_bound(lsh+1,lsh+cnt+1,x)-lsh;
+        y=lower_bound(lsh+1,lsh+cnt+1,y)-lsh;
+        ll res=0;
+        int l=0,r=0,lx=la[x],ly=la[y];
+        r=rr[y];
+        for(int j=1;j<=n;){
         	if(a[j]==y)r--;
             if(a[j]==x){l++;res=max(res,1ll*l*r);}
+            if(lx<=j)lx=nxt[lx];
+            if(ly<=j)ly=nxt[ly];
+            if(j<lx&&j<ly)j=min(lx,ly);
         }
         cout << res <<"\n";
     }
--- Competion/2023/10/2023_10_3/Senior1/fastslow/fastslow.cpp
+++ Competion/2023/10/2023_10_3/Senior1/fastslow/fastslow.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int N=1e5+7;
 int n,m;
 int a[N],lsh[N];
-int rr[N],nxt[N];
+int rr[N],nxt[N],la[N];
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -17,18 +17,27 @@ int main(){
     int cnt=unique(lsh+1,lsh+n+1)-lsh-1;
     for(int i=1;i<=n;i++)a[i]=lower_bound(lsh+1,lsh+cnt+1,a[i])-lsh;
 
-    for(int i=1;i<=n;i++)rr[a[i]]++;
-
+    for(int i=1;i<=n;i++){rr[a[i]]++;la[i]=n+1;}
+    for(int i=n;i>=1;i--){
+        nxt[i]=la[a[i]];
+        la[a[i]]=i;
+    }
+    nxt[0]=n+1;
+    
     for(int i=1;i<=m;i++){
         int x,y;
         cin >> x >> y;
         x=lower_bound(lsh+1,lsh+cnt+1,x)-lsh;
         y=lower_bound(lsh+1,lsh+cnt+1,y)-lsh;
-        ll res=0,l=0,r=0;
+        ll res=0;
+        int l=0,r=0,lx=la[x],ly=la[y];
         r=rr[y];
-        for(int j=1;j<=n;j++){
+        for(int j=1;j<=n;){
         	if(a[j]==y)r--;
             if(a[j]==x){l++;res=max(res,1ll*l*r);}
+            if(lx<=j)lx=nxt[lx];
+            if(ly<=j)ly=nxt[ly];
+            if(j<lx&&j<ly)j=min(lx,ly);
         }
         cout << res <<"\n";
     }
