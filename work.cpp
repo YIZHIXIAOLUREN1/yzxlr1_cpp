e308718da60dbffff494220f4164ccc63bc5eb01
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-10-04 13:58:02 +0800

    1

--- work.cpp
+++ work.cpp
@@ -1,31 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define mid ((l+r)>>1)
-const int N=1e18,M=1e9+7;
-int T;
-ll L,R;
-ll q,h,l,r;
+const int N=1e5+7;
 
-ll find(ll x){
-    l=1,r=M;
-    while(l<r){
-        if(mid*mid>=x)r=mid-1;
-        else l=mid+1;
-    }
-    while(l*l<x)l++;
-    while((l-1)*(l-1)>=x&&l>1)l--;
-    return l;
-}
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin >> T;
-    while(T--){
-        cin >> L >> R;
-        q=find(L)-1;
-        h=find(R);
-        if(h*h>R)h--;
-        cout << h-q << "\n";
-    }
-}   
+
+}
