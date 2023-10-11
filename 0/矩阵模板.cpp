a49e474933a44128d92a94b6fa08937cafeedea7
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-10-11 13:37:35 +0800

    1

--- 0/矩阵模板.cpp
+++ 0/矩阵模板.cpp
@@ -14,8 +14,8 @@ struct Matrix{
 	Matrix friend operator *(const Matrix &A,const Matrix &B){
 		Matrix Ans;Ans.clear();
 		for(int i=0;i<l;++i)
-			for(int j=0;j<l;++j)
-				for(int k=0;k<l;++k)
+			for(int k=0;k<l;++k)
+				for(int j=0;j<l;++j)
 					Ans.M[i][j]+=A.M[i][k]*B.M[k][j];
 		return Ans;
 	}
