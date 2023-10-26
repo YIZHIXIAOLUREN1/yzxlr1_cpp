6af7b571afd87421e1002114fadc5fc4ca66cf53
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-10-26 16:14:57 +0800

    1

---
+++ P2482.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=17,M=2007;
+int n,m;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    
+    return 0;
+}
