ca21748a6dbf4f3be129b3173a8d35ba32f3ac63
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-10-26 08:37:53 +0800

    1

--- Competion/2023/10/2023_10_21_csps2/SC-S00188/struct/struct.cpp
+++ Competion/2023/10/2023_10_21_csps2/SC-S00188/struct/struct.cpp
@@ -9,19 +9,6 @@ vector<int> zji;
 vector<int> zss;
 ll nle;
 
-string si(){
-	string s;
-	s="            ";
-	int len=0;
-	char ch=getchar();
-	while(ch<'a'||ch>'z')ch=getchar();
-	while((ch>='a'&&ch<='z')||ch=='.'){
-		s[len++]=ch;
-		ch=getchar();
-	}
-	return s;
-}
-
 bool vis[N];
 struct node{
     int id;
@@ -35,14 +22,135 @@ struct node{
 
 
 void csh();
-void cj();
-void my();
-void fy();
-void dj(){
+void cj1();
+void my2();
+void fy3();
+void dj4();
+
+int main(){
+    //freopen("struct.in","r",stdin);
+    //freopen("struct.out","w",stdout);
+    csh();
+    cin >> n;
+    while(n--){
+        ni++;
+        ll op;
+        cin >> op;
+        cout <<op<<":";//
+        if(op==1)cj1();
+        if(op==2)my2();
+        if(op==3)fy3();
+        if(op==4)dj4();
+    }
+
+}
+
+void csh(){
+    node by,st,it,lo;
+    by.id=101;by.sp=1;by.len=1;
+    st.id=102;st.sp=2;st.len=2;
+    it.id=103;it.sp=4;it.len=4;
+    lo.id=104;lo.sp=8;lo.len=8;
+    by.na="byte";st.na="short";
+	it.na="int";lo.na="long";
+    w[101]=by;w[102]=st;w[103]=it;w[104]=lo;
+    vis[104]=vis[103]=vis[102]=vis[101]=1;
+}
+
+void cj1(){
+    string s;int k;
+    //s=si();
+    cin>>s;
+    cin>>k;
+    node res;
+    res.na=s;res.id=ni;
+    res.sp=res.len=0;
+    vis[res.id]=1;
+    bool ff=0;
+    while(k--){
+        string t,tn;
+        cin>>t>>tn;
+        for(int i=104;i>=1;i--){
+            if(!vis[i])continue;
+            node v=w[i];
+            if(v.na==t){
+                res.son.push_back(v.id);
+                res.sna.push_back(tn);
+                res.sp=max(res.sp,v.sp);
+                if(res.len||ff)res.len=res.len+v.sp-res.len%v.sp;
+                ff=1;
+                res.sss.push_back(res.len);
+                res.len+=v.len-1;
+                break;
+            }
+        }
+    }
+    res.len=res.len+res.sp-res.len%res.sp-1;
+    w[res.id]=res;
+    cout << res.len+1 <<" "<<res.sp<<"\n";
+}
+
+void my2(){
+    string t,tn;
+    cin>>t>>tn;
+    for(int i=104;i>=1;i--){
+        if(!vis[i])continue;
+        node v=w[i];
+        if(v.na==t){
+            zjs.push_back(tn);
+            zji.push_back(v.id);
+            if(nle)nle=nle+v.sp-nle%v.sp;
+            cout << nle <<"\n";
+            zss.push_back(nle);
+            nle+=v.len-1;
+            break;
+        }
+    }
+}
+
+void fy3(){
+    string s;
+    cin>>s;
+    string ns;
+    ll res=0,f1=0;
+    node v;
+    for(int i=0;i<=s.size();i++){
+        if(i<s.size()&&s[i]!='.')ns+=s[i];
+        else if(!f1){
+        	//cout << ns<<"\n";//
+            for(int j=0;j<zjs.size();j++){
+                string t=zjs[j];
+                if(ns==t){
+                    res=zss[j];
+                    v=w[zji[j]];
+                    break;
+                }
+            }
+            ns.clear();
+            f1=1;
+        }else{
+        	//cout << ns<<"\n";//
+            for(int j=0;j<v.sna.size();j++){
+                string s=v.sna[j];
+                bool f=1;
+                if(ns!=s)f=0;
+                if(f){
+                    res=res+v.sss[j];
+                    v=w[v.son[j]];
+                }
+            }
+            ns.clear();
+        }
+    }
+    cout << res <<"\n";
+}
+
+
+void dj4(){
 	string ans="                                                            ";
 	ll alen=-1;
     ll adr;
-    cin >> adr;
+    cin>>adr;
     ll res=0,f1=0;
     node v;
     ll l=0;
@@ -89,148 +197,4 @@ void dj(){
     for(int i=0;i<ans.size();i++)if(ans[i]!=' ')cout<<ans[i];
     cout <<"\n";
     //cout << res <<"\n";//
-}
-
-int main(){
-    freopen("struct.in","r",stdin);
-    freopen("struct.out","w",stdout);
-    csh();
-    cin >> n;
-    while(n--){
-        ni++;
-        ll op;
-        cin >> op;
-        if(op==1)cj();
-        if(op==2)my();
-        if(op==3)fy();
-        if(op==4)dj();
-    }
-
-}
-
-void csh(){
-    node by,st,it,lo;
-    by.id=101;by.sp=1;by.len=1;
-    st.id=102;st.sp=2;st.len=2;
-    it.id=103;it.sp=4;it.len=4;
-    lo.id=104;lo.sp=8;lo.len=8;
-    by.na="byte        ";st.na="short       ";
-	it.na="int         ";lo.na="long        ";
-    w[101]=by;w[102]=st;w[103]=it;w[104]=lo;
-    vis[104]=vis[103]=vis[102]=vis[101]=1;
-}
-
-// const int m1=998244353,m2=1e9+7;
-// int hx2(string s){
-//     int r1,r2;
-//     for(int i=0;i<s.size();i++){
-//         r1+=s[i]-'a';r1%=m1;
-//         r2+=s[i]-'a';r2%=m2;
-//     }
-//     return r1,r2;
-// }
-
-void cj(){
-    string s;int k;
-    s=si();
-    cin >> k;
-    node res;
-    res.na=s;res.id=ni;
-    res.sp=res.len=0;
-    vis[res.id]=1;
-    bool ff=0;
-    while(k--){
-        string t,tn;
-        t=si();tn=si();
-        //cout << t << tn<<"\n";//
-        for(int i=104;i>=1;i--){
-            if(!vis[i])continue;
-            node v=w[i];
-            //cout << i << v.na<<"\n";//
-            if(v.na==t){
-                res.son.push_back(v.id);
-                res.sna.push_back(tn);
-                res.sp=max(res.sp,v.sp);
-                if(res.len||ff)res.len=res.len+v.sp-res.len%v.sp;
-                ff=1;
-                res.sss.push_back(res.len);
-                res.len+=v.len-1;
-                //cout << i << v.na<<v.len<<" "<< res.len<<"\n";//
-                break;
-            }
-        }
-    }
-    res.len=res.len+res.sp-res.len%res.sp-1;
-    w[res.id]=res;
-    cout << res.len+1 <<" "<<res.sp<<"\n";
-}
-
-void my(){
-    string t,tn;
-    t=si();tn=si();
-    for(int i=104;i>=1;i--){
-        if(!vis[i])continue;
-        node v=w[i];
-        if(v.na==t){
-            zjs.push_back(tn);
-            zji.push_back(v.id);
-            if(nle)nle=nle+v.sp-nle%v.sp;
-            cout << nle <<"\n";
-            zss.push_back(nle);
-            nle+=v.len-1;
-            break;
-        }
-    }
-}
-
-void fy(){
-    string s;
-    s=si();
-    char ns[12];
-    ll lns=-1,res=0,f1=0;
-    node v;
-    for(int i=0;i<12;i++){
-        if(s[i]!='.'&&s[i]!=' '){
-        	ns[++lns]=s[i];
-		}
-        else if(!f1){
-        	//cout << ns <<"\n";//
-            for(int j=0;j<zjs.size();j++){
-                string t=zjs[j];
-                bool f=1;
-                //cout << t<<":\n";//
-                for(int k=0;k<=lns;k++){
-                	//printf("%c:%c:%d\n",ns[k],t[k],ns[k]!=t[k]);//
-                	if(ns[k]!=t[k])f=0;
-				}
-                //cout <<f;//
-                if(lns<11&&t[lns+1]!=' ')f=0;
-                //cout <<f;//
-                if(f){
-                    res=zss[j];
-                    //printf("%d:\n",res);//
-                    v=w[zji[j]];
-                    break;
-                }
-            }
-            lns=-1;
-            f1=1;
-        }else{
-        	//cout << ns <<"\n";//
-            for(int j=0;j<v.sna.size();j++){
-                string s=v.sna[j];
-                bool f=1;
-                for(int k=0;k<=lns;k++)if(ns[k]!=s[k])f=0;
-                if(lns<11&&s[lns+1]!=' ')f=0;
-                if(f){
-                    res=res+v.sss[j];
-                    //printf("%d:\n",res);//
-                    v=w[v.son[j]];
-                }
-            }
-            lns=-1;
-        }
-        if(s[i]==' ')break;
-    }
-    cout << res <<"\n";
 }
