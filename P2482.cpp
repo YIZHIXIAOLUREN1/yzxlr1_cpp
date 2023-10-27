9b8315ea5e67f880830601ac1b6c98445ed2b979
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-10-27 12:59:14 +0800

    1

--- P2482.cpp
+++ P2482.cpp
@@ -1,10 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int N=17,M=2007;
+const int N=17,M=2007,MM=1e4+7;
 int n,m;
 int tcd[M],toc;//totcard
-queue<int> hdcd[N];//handcard
+//queue<int> hdcd[N];//handcard
+int hdcd[N][MM];
+bool vhd[N][MM];
+int hdr[N];
 //P桃0,K杀1,D闪2,F决3,N南4,W万5,J无懈6,Z连弩7
 int bld[N],bel[N],kw[N];//blood,belong,(0,zhu,1,zhong,2,f)
 //know(0,unknow,1,know,2,lf)
@@ -17,25 +20,42 @@ int jl(int user,int reser){
     while(now!=reser)now=r[now],res++;
     return res;
 }
-void gcd(int id,int num){//getcard
+void in(int user,int id){
+    hdcd[user][++hdr[user]]=id;
+    vhd[user][hdr[user]]=1;
+}
+int find(int user,int id){
+    for(int i=1;i<=hdr[user];i++)
+        if(vhd[user][i]&&hdcd[user][i]==id)return i;
+    return 0;
+}
+int fius(int user,int id){//find use
+    for(int i=1;i<=hdr[user];i++){
+        if(vhd[user][i]&&hdcd[user][i]==id){
+            vhd[user][i]=0;
+            return i;
+        }
+    }
+    return 0;
+}
+void gcd(int user,int num){//getcard
     while(num--){
         int cd;
         if(toc>0)cd=tcd[toc--];
         else cd=tcd[0];
-        hdcd[id].push(cd);
+        in(user,cd);
     }
 }
 void dead(int user,int reser){
-    if(!hdcd[reser].empty()&&hdcd[reser].front()==0){
+    if(fius(user,0)){
         bld[reser]++;
-        hdcd[reser].pop();
         return;
     }
     die[reser]=1;
     if(bel[reser]==2){
         gcd(user,3);
     }else if(bel[user]==0&&bel[reser]==1){
-        while(!hdcd[user].empty())hdcd[user].pop();
+        hdr[user]=0;
         zb[user]=0;
     }
 }
@@ -57,7 +77,7 @@ bool gx(int user,int reser,int rreser,int way){//是否无懈
 }
 bool wx(int user,int reser,int way){//way,0,dy,1,yq,2,zdy
     for(int rreser=user;rreser!=user;rreser=r[rreser]){
-        if(hdcd[rreser].empty()||hdcd[rreser].front()!=6)continue;
+        if(!find(rreser,6))continue;
         if(!gx(user,reser,rreser,way))continue;
         return use(6,rreser,reser);
     }
@@ -68,10 +88,8 @@ bool use(int id,int user,int reser){//id,来源,目标
         return 1;
     }else if(id==1){
         //kw[user][reser]=2;
-        if(!hdcd[reser].empty()&&hdcd[reser].front()==2){
-            hdcd[reser].pop();
-            return 0;
-        }else{
+        if(fius(reser,2))return 0;
+        else{
             bld[reser]--;
             if(bld[reser]==0)dead(user,reser);
             tf(user,reser);
@@ -79,10 +97,11 @@ bool use(int id,int user,int reser){//id,来源,目标
         }
     }else if(id==3){//!!!!
         int now=reser,bn=user;
-        while(!hdcd[now].empty()&&hdcd[now].front()==1){
-            hdcd[now].pop();
-            now=(now==user)?reser:user;
-            bn=(now==user)?reser:user;
+        if(bel[reser]!=1||bel[user]!=0){
+            while(fius(now,1)){
+                now=(now==user)?reser:user;
+                bn=(now==user)?reser:user;
+            }
         }
         tf(bn,now);
         bld[now]--;
@@ -106,14 +125,14 @@ int main(){
         else bel[i]=2;
         for(int j=1;j<=4;j++){
             cin >> car;
-            if(car=="P")hdcd[i].push(0);
-            else if(car=="K")hdcd[i].push(1);
-            else if(car=="D")hdcd[i].push(2);
-            else if(car=="F")hdcd[i].push(3);
-            else if(car=="N")hdcd[i].push(4);
-            else if(car=="W")hdcd[i].push(5);
-            else if(car=="J")hdcd[i].push(6);
-            else hdcd[i].push(7);
+            if(car=="P")in(i,0);
+            else if(car=="K")in(i,1);
+            else if(car=="D")in(i,2);
+            else if(car=="F")in(i,3);
+            else if(car=="N")in(i,4);
+            else if(car=="W")in(i,5);
+            else if(car=="J")in(i,6);
+            else in(i,7);
         }
     }
     
