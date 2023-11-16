7ce6e009f3060602882d6b3fa0b763ad5c28ed38
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-11-16 14:10:31 +0800

    1

--- work.cpp
+++ work.cpp
@@ -3,8 +3,8 @@ using namespace std;
 #define ll long long
 const int N=1e5+7;
 ll n,a[N],ans;
-
+map<int,string> p;
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cout<<(int)'m'-'a'<<" "<<(int)'l'-'a';
+    
 }
