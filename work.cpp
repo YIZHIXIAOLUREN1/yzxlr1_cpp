2abaca627bada5be956165ee3b09d2296b5eb2ca
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2023-12-24 10:23:50 +0800

    1

--- work.cpp
+++ work.cpp
@@ -1,10 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define db double
 const int N=1e5+7;
-ll n,a[N],ans;
+ll n;
+db a[N],ans;
 
 int main(){
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cout<<(int)' ';
+    cin>>n;
+    for(int i=1;i<=n;i++){cin>>a[i];ans+=a[i];}
+    cout<<ans/n;
 }
