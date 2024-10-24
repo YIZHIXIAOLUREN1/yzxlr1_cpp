af61bf2c9f2df2a078b1689877d612ee58a531b1
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2024-10-24 14:10:12 +0800

    1

--- redo/kmp.cpp
+++ redo/kmp.cpp
@@ -1,16 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define N 1000050
-int n,m,j;
-int kmp[N];
+#define ll long long
+const int N=1e6+7,mod=998244353;
 char s1[N],s2[N];
-
+int n,m;
+int kmp[N];
 
 int main(){
+    //ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     scanf("%s%s",s1+1,s2+1);
     n=strlen(s1+1);
     m=strlen(s2+1);
-    //cout << n << m;//
+    int j=0;
     for(int i=2;i<=m;i++){
         while(j&&s2[j+1]!=s2[i])j=kmp[j];
         if(s2[j+1]==s2[i])j++;
@@ -20,10 +21,9 @@ int main(){
     for(int i=1;i<=n;i++){
         while(j&&s2[j+1]!=s1[i])j=kmp[j];
         if(s2[j+1]==s1[i])j++;
-        if(j==m)printf("%d\n",i-m+1);
+        if(j==m)cout<<i-m+1<<"\n";
     }
-    for(int i=1;i<m;i++){
-        printf("%d ",kmp[i]);
-    }
-    printf("%d",kmp[m]);
+    for(int i=1;i<=m;i++)cout<<kmp[i]<<" \n"[i==m];
+    
+    return 0;
 }
---
+++ test/kmp_test/kmp_1.in
@@ -0,0 +1,2 @@
+aabaabaaf
+aabaaf
