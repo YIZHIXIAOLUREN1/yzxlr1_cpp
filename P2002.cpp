c7416f34b9e9f1f490ec0d5d6823760f5ac9cd75
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2024-11-20 13:52:15 +0800

    1

---
+++ P2002.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int N=1e6+7,mod=998244353;
+
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    
+    return 0;
+}
