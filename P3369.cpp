61eab0c0ebd48c56f648feadc7b3dc0e1b5c284d
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2024-11-27 14:15:56 +0800

    1

---
+++ P3369.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+//#include<bits/extc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+//using namespace __gnu_pbds;
+#define ll long long
+const int N=1e5+7,mod=998244353;
+__gnu_pbds::tree<int,__gnu_pbds::null_type,less<int>,
+__gnu_pbds::rb_tree_tag,
+__gnu_pbds::tree_order_statistics_node_update> rbtr;
+int n,x,opt;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cin>>opt>>x;
+        if(opt==1)rbtr.insert(x);
+        else if(opt==2)rbtr.erase(rbtr.lower_bound(x));
+        else if(opt==3)cout<<rbtr.order_of_key(x)+1<<"\n";
+        else if(opt==4)cout<<*rbtr.find_by_order(x-1)<<"\n";
+        else if(opt==5)cout<<*--rbtr.lower_bound(x)<<"\n";
+        else cout<<*rbtr.upper_bound(x)<<"\n";
+    }
+    return 0;
+}
--- atest0.cpp
+++ atest0.cpp
@@ -1,12 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-
-
+const int N=1e5+7,mod=998244353;
+int n,x,opt;
 
 int main(){
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cout <<0x3f3f3f3f;
+    ios::sync_with_stdio(0);cin.tie(0);
+    cin >> n;
+    while(n--){
+        cin>>opt>>x;
+
+    }
     
     return 0;
 }
+ 
