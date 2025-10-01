6a3013862f23cf3e32bb791bdf38eec075629527
YIZHIXIAOLUREN1 <yzxlr114514@outlook.com>
2025-10-01 13:55:09 +0800

    1

--- atest0.cpp
+++ atest0.cpp
@@ -1,29 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define N 1000050
-int n,m,j;
-int kmp[N];
-char s1[N],s2[N];
+#define ll long long
+const int N=1e5+7;
 
 
 int main(){
-    scanf("%s%s",s1+1,s2+1);
-    n=strlen(s1+1);
-    m=strlen(s2+1);
-    //cout << n << m;//
-    for(int i=2;i<=m;i++){
-        while(j&&s2[j+1]!=s2[i])j=kmp[j];
-        if(s2[j+1]==s2[i])j++;
-        kmp[i]=j;
-    }
-    j=0;
-    for(int i=1;i<=n;i++){
-        while(j&&s2[j+1]!=s1[i])j=kmp[j];
-        if(s2[j+1]==s1[i])j++;
-        if(j==m)printf("%d\n",i-m+1);
-    }
-    for(int i=1;i<m;i++){
-        printf("%d ",kmp[i]);
-    }
-    printf("%d",kmp[m]);
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
 }
